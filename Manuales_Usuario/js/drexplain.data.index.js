993810cfd23ef293d3695f45ad8b89e0c7c567bc
Diego Alexander Lozada Garcia <millerbsv@gmail.com>
2014-05-13 00:55:07 -0500

    Inicio del manual
    manual de usuario con insertar y búsquedas. Falta usuario, salir,
    modificar

---
+++ Manuales_Usuario/js/drexplain.data.index.js
@@ -0,0 +1,13 @@
+DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
+DR_EXPLAIN.data_index = {
+
+	// index
+	DREX_NODE_KEYWORDS: [1,2,3,4,9,10,5,8,7,6,9,10],
+	DREX_NODE_KEYWORDS_START: [0,1,2,3,6,7,8,9,10,11], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [1,2,3,6,7,8,9,10,11,12], //length:= drex.nodes_count,
+
+	DREX_KEYWORD_NAMES: ["<NUEVA PALABRA CLAVE>","Manual de usuario","Insertar","Elementos","Iconografia","Museo","Objetos","Colección","Cultura","Iconografía principal","Elementos iconográficos"],
+	DREX_KEYWORD_CHILD_START: [1,2,3,5,9,11,11,11,11,11,11],
+	DREX_KEYWORD_CHILD_END: [2,3,5,9,11,11,11,11,11,11,11]
+	
+};
