d8329ab7bb92b53126cf721a0c3e9de419a02c51
Diego Alexander Lozada Garcia <millerbsv@gmail.com>
2014-05-19 10:33:36 -0500

    Fin manual de usuario
    Se termino el manual de usuario y se creo el manual en web y pdf

---
+++ Manual de usuario/web/js/drexplain.data.index.js
@@ -0,0 +1,13 @@
+DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
+DR_EXPLAIN.data_index = {
+
+	// index
+	DREX_NODE_KEYWORDS: [1,2,3,4,9,10,3,4,9,10,5,8,7,6,9,10,5,8,7,6,9,10],
+	DREX_NODE_KEYWORDS_START: [0,1,2,2,2,2,2,3,6,6,6,6,6,6,7,10,11,12,13,14,15,16,16,16,16,17,18,19,20,21,22], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [1,2,2,2,2,2,3,6,6,6,6,6,6,7,10,11,12,13,14,15,16,16,16,16,17,18,19,20,21,22,22], //length:= drex.nodes_count,
+
+	DREX_KEYWORD_NAMES: ["<NUEVA PALABRA CLAVE>","Manual de usuario","Insertar","Elementos","Iconografia","Museo","Objetos","Colección","Cultura","Iconografía principal","Elementos iconográficos"],
+	DREX_KEYWORD_CHILD_START: [1,2,3,5,9,11,11,11,11,11,11],
+	DREX_KEYWORD_CHILD_END: [2,3,5,9,11,11,11,11,11,11,11]
+	
+};
